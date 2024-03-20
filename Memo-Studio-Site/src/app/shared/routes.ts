2fc2e7a151c6f6d7efcf31b19c24d60b2abe39b2
Adem <ademcran4aliev@abv.bg>
2024-03-20 22:43:50 +0200

    replcae address

--- Memo-Studio-Site/src/app/shared/routes.ts
+++ Memo-Studio-Site/src/app/shared/routes.ts
@@ -1,2 +1,2 @@
-    export const BASE_URL_DEV = "https://localhost:7190/api";
+    export const BASE_URL_DEV = "https://c1b3-89-215-182-166.ngrok-free.app/api";
     export const BASE_URL_PROD="https://c1b3-89-215-182-166.ngrok-free.app/api";
